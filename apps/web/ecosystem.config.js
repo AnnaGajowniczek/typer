216fc3eb218fc4dcb4c24dc3a170b1f74f496d91
Anna Gajowniczek <an.gajowniczek@gmail.com>
2026-06-09 15:04:45 +0200

    Dodanie ecosystem.config.js dla PM2 na Windows

---
+++ apps/web/ecosystem.config.js
@@ -0,0 +1,10 @@
+  module.exports = {
+    apps: [{
+      name: 'typowanie',
+      script: 'node_modules/next/dist/bin/next',                                                                               
+      args: 'start',
+      cwd: 'C:\\apps\\typowanie\\apps\\web',
+      interpreter: 'node'
+    }]
+  }                                                                                                                            
+  EOF
